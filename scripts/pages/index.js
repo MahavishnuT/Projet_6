eaa73058cdab93c5805e6df9c5b28ef8043802bc
Tom Pujalte <tom.pujalte@gmail.com>
2022-09-16 15:41:11 +0200

    fetching data and creating Photographer class

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,10 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return fetch("/data/photographers.json")
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(err => console.log("an error occurs", err));
     }
+
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -42,4 +23,3 @@
     };
     
     init();
-    
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,40 @@
-//Mettre le code JavaScript lié à la page photographer.html
+class Photographer {
+    
+    constructor(data) {
+        this._name = data.name
+        this._id = data.id
+        this._city = data.city
+        this._country = data.country
+        this._tagline = data.tagline
+        this._portrait = data.portrait
+        this._price = data.price
+    }
+
+    get id() {
+        return this._id
+    }
+
+    get name() {
+        return this._name
+    }
+
+    get city() {
+        return this._city
+    }
+
+    get country() {
+        return this._country
+    }
+
+    get tagline() {
+        return this._tagline
+    }
+
+    get price() {
+        return this._price
+    }
+
+    get portrait() {
+        return `/assets/photographers/Photographers_ID_Photos/${this._portrait}`
+    }
+}
